0597e35f18c52cfbda64371958e87fd4ffaf5365
Alexander Pagan <pagangod2323@gmail.com>
2017-02-06 18:31:43 -0600

    Added OutofBounds and BallSpawn
    Script OutofBounds for Ball to Respawn at point

---
+++ Assets/OutOfBounds.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour {
+    [SerializeField]Transform ballSpawn;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("Player"))
+            col.transform.position = ballSpawn.position;
+    }
+}
