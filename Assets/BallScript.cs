8883e95464b6272412fb50fd45fb36390fac7d5f
Alexander Pagan <pagangod2323@gmail.com>
2017-02-07 02:09:13 -0600

    Added Score script and More
    Pinball now keeps score based on which triggers and bumpers it hits

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallScript : MonoBehaviour
 {
     Rigidbody2D Ball;
     Vector2 old;
+    public Text ScoreText;
+    private int Score;
 
     void Start()
     {
         Ball = GetComponent<Rigidbody2D>();
+        Score = 0;
+        
     }
 
     private void FixedUpdate()
@@ -23,9 +28,21 @@ public class BallScript : MonoBehaviour
         {
             ContactPoint2D cont = col.contacts[0];
             Ball.velocity = Vector2.Reflect(old, cont.normal); //Vector 2 reflects back to the normal
-
             Ball.velocity += cont.normal * 3.0f;  //Speeds ball up 
-
+            Score = Score + 100;
+            SetScoreText();
         }
+        if (col.transform.CompareTag("Trigger"))
+        {
+            ContactPoint2D cont = col.contacts[0];
+            Ball.velocity = Vector2.Reflect(old, cont.normal); //Vector 2 reflects back to the normal
+            Ball.velocity += cont.normal * 3.0f;  //Speeds ball up 
+            Score = Score + 50;
+            SetScoreText();
+        }
+    }
+    void SetScoreText()
+    {
+        ScoreText.text = "SCORE: " + Score.ToString();
     }
 }
